3ad961b1d18d4b7032550dcdffa4e23dbb1b2420
Jacek Kobus <kobus.jacek@gmail.com>
2014-09-25 23:50:35 +0200

    added PriorityTest updated build.xml updated gitignore

---
+++ tests/fixtures/PHPExtra/EventManager/PriorityTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace fixtures\PHPExtra\EventManager;
+
+use PHPExtra\EventManager\Priority;
+
+/**
+ * The PriorityTest class
+ *
+ * @author Jacek Kobus <kobus.jacek@gmail.com>
+ */
+class PriorityTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @return array
+     */
+    public function nameToValue()
+    {
+        return array(
+            array('lowest', -1000),
+            array('low', -500),
+            array('normal', 0),
+            array('high', 500),
+            array('highest', 1000),
+            array('monitor', ~PHP_INT_MAX),
+        );
+    }
+
+    /**
+     * @dataProvider nameToValue
+     *
+     * @param string $name
+     * @param int    $expectedValue
+     */
+    public function testGivenPriorityValueReturnItsName($name, $expectedValue)
+    {
+        $value = Priority::getPriorityByName($name);
+        $this->assertEquals($expectedValue, $value);
+    }
+
+    /**
+     * @dataProvider nameToValue
+     *
+     * @param string $expectedName
+     * @param int    $value
+     */
+    public function testGivenValidPriorityNameReturnItsIntegerValue($expectedName, $value)
+    {
+        $name = Priority::getPriorityName($value);
+        $this->assertEquals($expectedName, $name);
+    }
+}
+ 
